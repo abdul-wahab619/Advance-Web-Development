27c200d9606c3f6c7215ed90ea02334f8364c760
abdul-wahab619 <awminhas619@gmail.com>
2024-04-22 23:47:05 +0500

    Exercise-4 Displaying Fetched Data

--- rest-api-02/src/App.jsx
+++ rest-api-02/src/App.jsx
@@ -40,33 +40,25 @@ function App() {
             key={index}
             className="border-2 border-gray-500 rounded-lg px-4 py-2 m-4 relative hover:shadow-xl"
           >
-            <h2 className=" top-1 right-2 px-4 py-1 bg-red-300 rounded-lg">
-              {article.publishedAt}
-            </h2>
+            <div className="top-1 w-full">
+              <img
+                className="w-full h-auto rounded-lg"
+                src={article.urlToImage}
+                alt={article.title}
+              />
+            </div>
+
             <h4 className="my-2 text-gray-500">{article.source[1]}</h4>
             <div className="flex justify-start items-center gap-x-2">
-              <PiBookOpenTextLight className="text-red-300 text-2xl" />
               <h2 className="my-1">{article.title}</h2>
             </div>
             <div className="flex justify-start items-center gap-x-2">
               <BiUserCircle className="text-red-300 text-2xl" />
               <h2 className="my-1">{article.author}</h2>
             </div>
-            <div className="flex justify-between items-center gap-x-2 mt-4 p-4">
-              <BiShow
-                className="text-3xl text-blue-800 hover:text-black cursor-pointer"
-                onClick={() => setShowModal(true)}
-              />
-              <Link to={`/books/details/${article._id}`}>
-                <BsInfoCircle className="text-2xl text-green-800 hover:text-black" />
-              </Link>
-              <Link to={`/books/edit/${article._id}`}>
-                <AiOutlineEdit className="text-2xl text-yellow-600 hover:text-black" />
-              </Link>
-              <Link to={`/books/delete/${article._id}`}>
-                <MdOutlineDelete className="text-2xl text-red-600 hover:text-black" />
-              </Link>
-            </div>
+            <h2 className=" top-1 right-2 px-4 py-1 bg-red-300 rounded-lg">
+              {article.publishedAt}
+            </h2>
           </div>
         ))}
       </div>
