8c8ca1d8a7c9d2c084d58795cc79690003b60141
abdul-wahab619 <awminhas619@gmail.com>
2024-04-30 23:25:31 +0500

    Apply Pagination Feature into it

--- rest-api-02/src/App.jsx
+++ rest-api-02/src/App.jsx
@@ -1,26 +1,25 @@
 import React, { useState, useEffect } from "react";
-import { BrowserRouter as Router, Link } from "react-router-dom";
-import { PiBookOpenTextLight } from "react-icons/pi";
-import { BiUserCircle, BiShow } from "react-icons/bi";
-import { AiOutlineEdit } from "react-icons/ai";
-import { BsInfoCircle } from "react-icons/bs";
-import { MdOutlineDelete } from "react-icons/md";
+import { BrowserRouter as Router } from "react-router-dom";
+import { BiUserCircle } from "react-icons/bi";
+
 import "./App.css";
 
 function App() {
   const [articles, setArticles] = useState([]);
   const [totalResults, setTotalResults] = useState(0);
-  const [showModal, setShowModal] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [pageSize, setPageSize] = useState(6);
+  const [totalPages, setTotalPages] = useState(0);
 
   const getDataFromApi = async () => {
-    let myEndpoint =
-      "https://newsapi.org/v2/top-headlines?country=us&apiKey=69fc58c00ae84add99a19037b609b31e";
+    const myEndpoint = `https://newsapi.org/v2/top-headlines?country=us&apiKey=69fc58c00ae84add99a19037b609b31e&page=${currentPage}&pageSize=${pageSize}`;
+
     try {
-      let response = await fetch(myEndpoint);
-      let data = await response.json();
+      const response = await fetch(myEndpoint);
+      const data = await response.json();
+      const totalPages = Math.ceil(data.totalResults / pageSize);
 
-      console.log("data", data);
-
+      setTotalPages(totalPages);
       setTotalResults(data.totalResults);
       setArticles(data.articles);
     } catch (error) {
@@ -28,9 +27,21 @@ function App() {
     }
   };
 
+  const handleNextPage = async () => {
+    if (currentPage < totalPages) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
+  const handlePreviousPage = async () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
   useEffect(() => {
     getDataFromApi();
-  }, []);
+  }, [currentPage, pageSize]);
 
   return (
     <Router>
@@ -43,24 +54,48 @@ function App() {
             <div className="top-1 w-full">
               <img
                 className="w-full h-auto rounded-lg"
-                src={article.urlToImage}
+                src={article.urlToImage || "default_image_url"}
                 alt={article.title}
               />
             </div>
-
-            <h4 className="my-2 text-gray-500">{article.source[1]}</h4>
+            <h4 className="my-2 text-gray-500">{article.source?.name}</h4>
             <div className="flex justify-start items-center gap-x-2">
               <h2 className="my-1">{article.title}</h2>
             </div>
             <div className="flex justify-start items-center gap-x-2">
               <BiUserCircle className="text-red-300 text-2xl" />
-              <h2 className="my-1">{article.author}</h2>
+              <h2 className="my-1">{article.author || "Unknown Author"}</h2>
             </div>
             <h2 className=" top-1 right-2 px-4 py-1 bg-red-300 rounded-lg">
               {article.publishedAt}
             </h2>
           </div>
         ))}
+      </div>
+      <div className="flex justify-center align-middle gap-5 m-3 pagination">
+        <button
+          onClick={handlePreviousPage}
+          disabled={currentPage === 1}
+          className={`bg-blue-400 text-white font-bold py-3 px-4 rounded-full  ${
+            currentPage === 1 ? "bg-blue-200" : "bg-blue-400"
+          }`}
+        >
+          Previous
+        </button>
+        <p
+          className={`bg-blue-400 text-white font-bold py-3 px-4 rounded-full`}
+        >
+          Page {currentPage} of {totalPages}
+        </p>
+        <button
+          onClick={handleNextPage}
+          disabled={currentPage === totalPages}
+          className={`bg-blue-400 text-white font-bold py-3 px-4 rounded-full ${
+            currentPage === totalPages ? "bg-blue-200" : "bg-blue-400"
+          }`}
+        >
+          Next
+        </button>
       </div>
     </Router>
   );
